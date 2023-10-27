0fe248d0edd12f2e57168c874c95c7e7fb1e1c30
pablopinto85 <pablo.pinto1985@gmail.com>
2023-10-27 16:49:13 -0300

    subida de aRchivos

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require("express-session")
+const handlebars = require("express-handlebars")
+const sessionRouter = require("./routes/sessions")
+const viewsRouter = require("./routes/views")
+const MongoStore = require("connect-mongo")
+const path = require ("path")
+
+const port = 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://pablopinto1985:pablo1985@ecommerce.kyhfmlv.mongodb.net/?retryWrites=true&w=majority",
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        ttl: 1000
+    }),
+    secret: "coderhouse",
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main", extname: ".handlebars" }))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+
+
+app.use("/api/sessions", sessionRouter)
+app.use("/", viewsRouter)
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
