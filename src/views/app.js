2f895554c0b3a8fa2ec0ea8996dd1f8834f931ad
pablopinto85 <pablo.pinto1985@gmail.com>
2023-10-28 03:03:18 -0300

    solucionado problemas de conexion con momgodb

--- src/views/app.js
+++ src/views/app.js
@@ -4,6 +4,7 @@ const handlebars = require("express-handlebars")
 const sessionRouter = require("../routes/sessions.js")
 const viewsRouter = require("../routes/views.js")
 const MongoStore = require("connect-mongo")
+const mongoose = require("mongoose")
 const path = require ("path")
 
 const port = 8080;
@@ -14,13 +15,28 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({
     store: MongoStore.create({
         mongoUrl: "mongodb+srv://pablopinto1985:pablo1985@ecommerce.kyhfmlv.mongodb.net/?retryWrites=true&w=majority",
-        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        mongoOptions: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // Configuración para habilitar SSL y deshabilitar la validación SSL (sslValidate: false)
+            ssl: true,
+            sslValidate: false
+        },
         ttl: 1000
     }),
     secret: "coderhouse",
     resave: true,
     saveUninitialized: true
-}))
+}));
+
+
+mongoose.connect("mongodb+srv://pablopinto1985:pablo1985@ecommerce.kyhfmlv.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+    ssl: true,
+    sslValidate: false
+});
 
 app.engine("handlebars", handlebars.engine({ defaultLayout: "main", extname: ".handlebars" }))
 app.set("views", path.join(__dirname, "views"));
